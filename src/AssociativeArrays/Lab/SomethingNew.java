6b299ac8184618a3bc0ec877f50af06a48e9cfd5
Yordan Stanchev <yordan.stanchev1998@gmail.com>
2023-11-30 20:22:15 +0200

    test za Plamen

---
+++ src/AssociativeArrays/Lab/SomethingNew.java
@@ -0,0 +1,18 @@
+package AssociativeArrays.Lab;
+
+import java.util.Scanner;
+
+public class SomethingNew {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = "Who's a faggot?";
+        System.out.println(text);
+
+        String pedal = "PLamen";
+        String additional = " is a faggot!";
+
+        System.out.println(pedal);
+        System.out.println(pedal + additional);
+    }
+}
